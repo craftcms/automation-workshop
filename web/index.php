19e3cc6fc40d6a7ee14f8a2a37a149cb997ee8cc
Tim Kelty <tim@craftcms.com>
2022-09-25 13:41:09 -0400

    No Key

--- web/index.php
+++ web/index.php
@@ -6,10 +6,6 @@
 // Load shared bootstrap
 require dirname(__DIR__) . '/bootstrap.php';
 
-if ($cmsLicenseKey = craft\helpers\App::env('LICENSE_KEY')) {
-    define('CRAFT_LICENSE_KEY', $cmsLicenseKey);
-}
-
 // Load and run Craft
 /** @var craft\web\Application $app */
 $app = require CRAFT_VENDOR_PATH . '/craftcms/cms/bootstrap/web.php';
