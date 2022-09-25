f3548b7e3d882bae1d7d44f10820d7b6fef9e03a
Tim Kelty <tim@craftcms.com>
2022-09-25 13:26:51 -0400

    More defensive

--- config/apple-news.php
+++ config/apple-news.php
@@ -2,13 +2,18 @@
 
 use craft\helpers\App;
 
+$channels = [];
+$channelId = App::env('NEWS_CHANNEL_ID');
+
+if ($channelId) {
+    $channels[] = [
+        'class' => applenewschannels\AppleFakeNews::class,
+        'channelId' => $channelId,
+        'apiKeyId' => (string) App::env('NEWS_API_KEY'),
+        'apiSecret' => (string) App::env('NEWS_API_SECRET')
+    ];
+}
+
 return [
-    'channels' => [
-        [
-            'class' => applenewschannels\AppleFakeNews::class,
-            'channelId' => App::env('NEWS_CHANNEL_ID'),
-            'apiKeyId' => App::env('NEWS_API_KEY'),
-            'apiSecret' => App::env('NEWS_API_SECRET')
-        ],
-    ],
+    'channels' => $channels,
 ];
